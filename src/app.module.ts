45fe5b59e2942ed9749b7988e92568d5ec2e3279
Kl3dJ <bogdankardash50@gmail.com>
2026-03-27 21:47:43 +0200

    Added connection to mongoDB, feat/tours

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,43 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ToursController } from './tours/tours.controller';
+import { ToursService } from './tours/tours.service';
+import { ToursModule } from './tours/tours.module';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ToursModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'config.env' }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const db = configService.get<string>('DATABASE');
+        const password = configService.get<string>('DATABASE_PASSWORD');
+
+        if (!db || !password) {
+          throw new Error('Database config is missing');
+        }
+
+        const uri = db.replace('<PASSWORD>', password);
+
+        return {
+          uri,
+          onConnectionCreate: (connection: Connection) => {
+            connection.on('connected', () =>
+              console.log('--------THIS SHIT IS WORKING DAMMIT!!!-------'),
+            );
+
+            return connection;
+          },
+        };
+      },
+    }),
+  ],
+  controllers: [AppController, ToursController],
+  providers: [AppService, ToursService],
 })
 export class AppModule {}
