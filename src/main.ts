911055c9527b8693c3ff4137a379604c20531259
Bohdan Kardash <82287413+Kl3dJ@users.noreply.github.com>
2026-03-30 18:27:48 +0300

    Utils (#7)
    * feat/auth
    
    * minor fix
    
    * fixed 401 tours
    
    * feat/mongoDB filter
    
    * minor fix/utils

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MongoExceptionFilter } from './utils/mongo-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  app.useGlobalFilters(new MongoExceptionFilter());
 
   app.setGlobalPrefix('api/v1');
 
