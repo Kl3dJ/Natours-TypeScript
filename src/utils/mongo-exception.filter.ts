6a46f622243e7443384e6e4ac6d3cc15182def8f
Kl3dJ <bogdankardash50@gmail.com>
2026-03-30 18:18:00 +0300

    feat/mongoDB filter

---
+++ src/utils/mongo-exception.filter.ts
@@ -0,0 +1,31 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  ConflictException,
+} from '@nestjs/common';
+import { MongoServerError } from 'mongodb';
+
+@Catch(MongoServerError)
+export class MongoExceptionFilter implements ExceptionFilter {
+  catch(exception: MongoServerError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    if (exception.code === 11000) {
+      const field = Object.keys(exception.keyValue)[0];
+      const value = exception;
+      // Duplicate key error
+      response.status(409).json({
+        status: 'error',
+        message: `${field} with value '${value}' already exists.`,
+      });
+    } else {
+      // For other MongoDB errors, you can handle them as needed
+      response.status(500).json({
+        status: 'error',
+        message: 'An unexpected error occurred.',
+      });
+    }
+  }
+}
