14bbf39ee7f4045e65407cc6839a6cfbc846cf8f
Kl3dJ <bogdankardash50@gmail.com>
2026-03-30 15:15:47 +0300

    user update

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
@@ -10,7 +10,7 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async findAll() {
-    return this.userModel.find();
+    return this.userModel.find({ active: true });
   }
 
   async findById(id: string) {
@@ -18,7 +18,12 @@ export class UsersService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    const { password, ...rest } = createUserDto;
+    const { password, passwordConfirm, ...rest } = createUserDto;
+
+    if (password !== passwordConfirm) {
+      throw new BadRequestException('Passwords do not match');
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const createdUser = new this.userModel({
       ...rest,
@@ -27,7 +32,24 @@ export class UsersService {
     return createdUser.save();
   }
 
-  async findByEmail(email: string) {
-    return this.userModel.findOne({ email });
+  async updateUser(id: string, updateUserDto: Partial<CreateUserDto>) {
+    if (updateUserDto.password && updateUserDto.passwordConfirm) {
+      throw new Error(
+        'This route is not for password updates. Please use /updateMyPassword.',
+      );
+    }
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+  }
+
+  async deleteMe(id: string) {
+    await this.userModel.findByIdAndUpdate(
+      id,
+      { active: false },
+      { returnDocument: 'after' },
+    );
+    return {
+      status: 'success',
+      message: 'User deactivated successfully',
+    };
   }
 }
