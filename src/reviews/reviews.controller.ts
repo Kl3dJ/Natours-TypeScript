cee032eed9529eb5e488a07d23f5c05853f6e320
Kl3dJ <bogdankardash50@gmail.com>
2026-03-31 14:08:36 +0300

    crud reviews

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ReviewsService } from './reviews.service';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { Public } from '../auth/public.decorator';
 
 @Controller('reviews')
-export class ReviewsController {}
+export class ReviewsController {
+  constructor(private readonly reviewsService: ReviewsService) {}
+
+  @Public()
+  @Get()
+  async findAll() {
+    return this.reviewsService.findAll();
+  }
+
+  @Public()
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.reviewsService.findOne(id);
+  }
+}
