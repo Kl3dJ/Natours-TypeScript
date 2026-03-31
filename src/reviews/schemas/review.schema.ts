39df8c27fb2dc157b89aa8d9ffba611548839159
Kl3dJ <bogdankardash50@gmail.com>
2026-03-31 13:49:00 +0300

    defined review schema

---
+++ src/reviews/schemas/review.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Review {
+  @Prop({ required: true })
+  review: string;
+
+  @Prop({ required: true, min: 1, max: 5 })
+  rating: number;
+
+  @Prop({ default: Date.now() })
+  createdAt: Date;
+
+  @Prop({ required: true, ref: 'Tour' })
+  tour: string;
+
+  @Prop({ required: true, ref: 'User' })
+  user: string;
+}
+
+export type ReviewDocument = Review & Document;
+export const ReviewSchema = SchemaFactory.createForClass(Review);
